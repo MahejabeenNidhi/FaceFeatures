820c8a62eb8e8e2da0e8e07e28d8da225c5f6351
Mahejabeen H Nidhi <52653630+MahejabeenNidhi@users.noreply.github.com>
2021-07-19 16:25:00 +0800

    Delete RCPR directory

--- RCPR/codefromCOFW/rcprCompile.m
+++
@@ -1,9 +0,0 @@
-%COMPILE ALL .c objective functions
-disp('Compiling.......................................');
-cd private
-mex fernsInds2.c
-mex maxCov1.c
-mex selectCorrFeat1.c
-mex stdFtrs1.c
-cd ..
-disp('DONE');
